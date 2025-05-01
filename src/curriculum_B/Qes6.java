967e9fbc37ab35fc128e87b341d9138fa457d0d1
mayuc <mayuc@192.168.0.14>
2025-05-01 10:13:14 +0900

    java1.16(カリキュラムB)

--- src/curriculum_B/Qes6.java
+++ src/curriculum_B/Qes6.java
@@ -3,56 +3,68 @@ package curriculum_B;
 import java.util.Scanner;
 
 public class Qes6 {
-	
+
 	public static void main(String[] args) {
-	
-	/*6
-	 * 入力した商品の残り台数が出力されるシステムを下記の条件で作成してください
-	 */
-	
-	//入力例↓↓
-	//パソコン、冷蔵庫、扇風機、洗濯機、加湿器、テレビ、ディスプレイ、その他商品
-	
-	System.out.print("商品を入力してください");
-	
-	 Scanner scan = new Scanner(System.in);
+
+		/*6
+		 * 入力した商品の残り台数が出力されるシステムを下記の条件で作成してください
+		 */
+
+		//入力例↓↓
+		//パソコン、冷蔵庫、扇風機、洗濯機、加湿器、テレビ、ディスプレイ、その他商品
+
+		System.out.print("商品を入力してください");
+
+		// ユーザーの入力を受け取る
+		Scanner scan = new Scanner(System.in);
+		// 入力された値をitemに代入する
 		String item = scan.nextLine();
-		
-	   String[] itemBox = item.split("、");
-	   int tvRandom = new java.util.Random().nextInt(12);
-	   
-	   for(String items : itemBox) {
-		   int nokori = new java.util.Random().nextInt(12);
-		   
-		   switch (items) {
-		   
-		   //パソコン、冷蔵庫、扇風機、洗濯機、加湿器　の残り台数は〇台です
-		   case"パソコン":
-		   case"冷蔵庫":
-		   case"扇風機":
-		   case"洗濯機":
-		   case"加湿器":
-			
-		     System.out.println(items +"の残り台数は"+nokori+"台です");   
-		   
-		   break;
-		   
-		 //テレビ、ディスプレイ　の残り台数は〇台です
-		   case"テレビ":
-		   case"ディスプレイ":
-			   
-		     System.out.println(items +"の残り台数は"+tvRandom+"台です"); 
-			   
-		   break;
-			  
-		 //『 その他商品 』は指定の商品ではありません
-		   default:
-				   
-			  System.out.println("『"+ items +"』は指定の商品ではありません");
-			
-		   }
-	   }
+
+		// itemをカンマで区切って配列に入れる
+		String[] itemBox = item.split("、");
+		// テレビ、ディスプレイの残り台数を0~11の乱数で決めて、tvRandomに代入する
+		int tvRandom = new java.util.Random().nextInt(12);
+
+		// 拡張for文で、入力された文字を一つずつ出力していく
+		for(String items : itemBox) {
+			// パソコン、冷蔵庫、扇風機、洗濯機、加湿器の残り台数を0~11の乱数で決めて、nokoriに代入する
+			int nokori = new java.util.Random().nextInt(12);
+
+			//switch文で分岐を作ってそれぞれの処理を実行する
+			switch (items) {
+
+			//パソコン、冷蔵庫、扇風機、洗濯機、加湿器　の残り台数は〇台です
+			// パソコン、冷蔵庫、扇風機、洗濯機、加湿器 の場合の処理
+			case"パソコン":
+			case"冷蔵庫":
+			case"扇風機":
+			case"洗濯機":
+			case"加湿器":
+
+				System.out.println(items +"の残り台数は"+nokori+"台です");   
+
+				// switchを抜ける
+				break;
+
+				// テレビ、ディスプレイ　の残り台数は〇台です
+				// テレビ、ディスプレイの場合の処理
+			case"テレビ":
+			case"ディスプレイ":
+
+				System.out.println(items +"の残り台数は"+tvRandom+"台です"); 
+
+				// switchを抜ける
+				break;
+
+			//『 その他商品 』は指定の商品ではありません
+			// パソコン、冷蔵庫、扇風機、洗濯機、加湿器、テレビ、ディスプレイ以外の場合
+			default:
+
+				System.out.println("『"+ items +"』は指定の商品ではありません");
+
+			}
+		}
 	}
 }
-	
+
 
--- src/curriculum_B/Qes7.java
+++ src/curriculum_B/Qes7.java
@@ -3,76 +3,105 @@ package curriculum_B;
 import java.util.Scanner;
 
 public class Qes7 {
-	
+
 	public static void main(String[] args) {
-	
-	/*7
-	 * N人の生徒の成績を管理するプログラムを下記条件で作成してください
-	 */
-	
-	//生徒の人数を入力してください（2以上）: 2
+
+		/*7
+		 * N人の生徒の成績を管理するプログラムを下記条件で作成してください
+		 */
+
+		// 生徒の人数を入力してください（2以上）: 2
 		System.out.print("生徒の人数を入力してください(2以上):");
-	
-        Scanner scan = new Scanner(System.in);
+
+		// ユーザーの入力を受け取る
+		Scanner scan = new Scanner(System.in);
+		// 入力された値をstudentsに代入する
 		int students = scan.nextInt();
-		
-		//〇人目の『英語』の点数を入力してください :10
-		//〇人目の『数学』の点数を入力してください :10
-		//〇人目の『理科』の点数を入力してください :10
-		//〇人目の『社会』の点数を入力してください :10
-		
+
+		// 〇人目の『英語』の点数を入力してください :10
+		// 〇人目の『数学』の点数を入力してください :10
+		// 〇人目の『理科』の点数を入力してください :10
+		// 〇人目の『社会』の点数を入力してください :10
+
+		// 生徒数の配列
 		int [] studentBox = new int[students];
+		// 科目の配列
 		String [] kamoku = {"英語","数学","理科","社会"};
+		// 生徒と教科の二次元配列 // kamoku.length→科目数
 		int [][]tensuu =new int [students][kamoku.length];
 
+		// 生徒をカウントする
 		int i ;
+		// 科目をカウントする
 		int j ;
-		
-	    for (i=0; i<studentBox.length;i++) {
-	    	
-	    	studentBox[i] = i+1;
-	    	
-	    	for(j=0;j<kamoku.length;j++){
-	    		
-	    	System.out.print(studentBox[i]+"人目の『"+kamoku[j]+"』の点数を入力してください:");
-	    	
-	    	tensuu[i][j]= scan.nextInt();
-	    	
-	     }
-	    }
-	  //〇人目の平均点は10.00点です。
-		
-	    	 for (i=0; i<studentBox.length;i++) {
-		    	 int total = 0 ;	
-		    	for(j=0;j<kamoku.length;j++){
-		        total += tensuu[i][j];
-		    	
-		    	}
-		    	
-	   	     double heikin = (double)total / kamoku.length ;
-	   	     System.out.printf(studentBox[i]+"人目の平均点は%.2f点です。",heikin);	
-	   	     System.out.println();
-	   	   
-	         }
-	    	 
-	    	 //〇〇の平均点は10.00点です。		 
-	    		 int subTotal = 0;
-	    		 for(j=0;j<kamoku.length;j++){
-	    		 for (i=0; i<studentBox.length;i++) {
-	    			 
-	    		subTotal += tensuu[i][j];	 
-	    		
-	    		 }
-	    		 
-	    		 double subHeikin = (double)subTotal / students;
-	    		 System.out.printf(kamoku[j]+"の平均点は%.2f点です。",subHeikin);	
-	    		 System.out.println();
-	    		 
-	    	} 
-	       }
-          }
 
-	
-	
-	
+		// 生徒をカウント
+		for (i=0; i<studentBox.length;i++) {
 
+			// studentBox[i]は0から始まるから、+1にして人数をカウントする
+			studentBox[i] = i+1;
+
+			// 科目をカウント（一人ずつ順番に科目の点数が入力できるようにする）
+			for(j=0;j<kamoku.length;j++){
+
+				// studentBox[i]で何人目か、kamoku[j]で科目を順番に出力していく
+				System.out.print(studentBox[i]+"人目の『"+kamoku[j]+"』の点数を入力してください:");
+
+				// 生徒と教科ごとに入力された点数をtensuu[i][j]に代入する
+				tensuu[i][j]= scan.nextInt();
+
+			}
+		}
+		//〇人目の平均点は10.00点です。
+
+		// 生徒をカウント
+		for (i=0; i<studentBox.length;i++) {
+			// 点数の合計を初期化する（0）
+			int total = 0 ;	
+			// 科目をカウント 
+			for(j=0;j<kamoku.length;j++){
+				// totalに点数を足していって合計点を出す
+				total += tensuu[i][j];
+			}
+			// 点数の合計（total）を科目数（kamoku.length）で割って平均点を出す
+			// 小数点第二位まで表示するため、int型をdouble型に変更する
+			double heikin = (double)total / kamoku.length ;
+			// printf("%f〇",△△)で少数点第二位まで出力する
+			System.out.printf(studentBox[i]+"人目の平均点は%.2f点です。",heikin);	
+			// 改行
+			System.out.println();
+		}
+		//〇〇の平均点は10.00点です。	
+		//科目をカウント
+		for(j=0;j<kamoku.length;j++){
+			// 科目の合計点の初期化（0）
+			int subTotal = 0;
+			// 生徒をカウント
+			for (i=0; i<studentBox.length;i++) {
+				// subTotalに科目ごとの点数を足して合計点を出す
+				subTotal += tensuu[i][j];
+			}
+			// 科目ごとの合計点（subTotal）を人数（students）で割って平均点を出す
+			// 小数点第二位まで表示するため、int型をdouble型に変更する
+			double subHeikin = (double)subTotal / students;
+			// printf("%f〇",△△)で少数点第二位まで出力する
+			System.out.printf(kamoku[j]+"の平均点は%.2f点です。",subHeikin);	
+			// 改行
+			System.out.println();
+		}
+		// 全体の合計点の初期値を設定（0）
+		int all = 0;
+		// 科目をカウント
+		for(j=0;j<kamoku.length;j++){
+			// 生徒をカウント
+			for (i=0; i<studentBox.length;i++) {
+				// 全部の点数を足す
+				all += tensuu[i][j];
+			}
+		}
+		// 全部の点数を人数×科目数で割って平均点を出す
+		double allHeikin = (double)all / (students*kamoku.length);
+		// printf("%f〇",△△)で少数点第二位まで出力する
+		System.out.printf("全体の平均点は%.2f点です。",allHeikin);	
+	}
+}
