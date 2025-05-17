bf14867ae559176e58ec5716bfd17bd175c47a39
mayuc <mayuc@192.168.0.14>
2025-05-17 22:38:48 +0900

    java1.19

--- src/basicClass/Animal.java
+++ src/basicClass/Animal.java
@@ -8,31 +8,31 @@ import java.util.Date;
 public class Animal {
 
 	public static void main(String[] args) {
-		
+
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		// Dogクラスをdog変数に代入
 		Dog dog = new Dog ();
 		// DogクラスのanimalNameをコンソールに出力する
 		System.out.println("動物の名前："+dog.animalName);
-		
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		// DogクラスをdogNum変数に代入。引数に値を渡す。
 		Dog dogNum = new Dog (2);
 		// dogNum.animalNumで動物の数を出力する
 		System.out.println("動物の数："+dogNum.animalNum);
-				
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		// SimpleDateFormatをインスタンス化してコンストラクタを呼ぶ（日付のフォーマットを指定）
-        SimpleDateFormat shosiki = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+		SimpleDateFormat shosiki = new SimpleDateFormat("yyyy-MM-dd H:m:s");
 
-        // 現在の日時を変数now
-        Date now = new Date();
+		// 現在の日時を変数now
+		Date now = new Date();
 
-        // nowを指定したフォーマットに合わせた値をformattedDateに代入
-        String formatNow = shosiki.format(now);
+		// nowを指定したフォーマットに合わせた値をformattedDateに代入
+		String formatNow = shosiki.format(now);
 
-        // formattedDateを出力
-        System.out.println("現在の日時：" + formatNow);
+		// formattedDateを出力
+		System.out.println("現在の日時：" + formatNow);
 
 	}
 
--- src/basicClass/Dog.java
+++ src/basicClass/Dog.java
@@ -2,28 +2,28 @@ package basicClass;
 
 public class Dog {
 
-		
-		// Q1：フィールドに動物の名前の変数を定義してください。
-		String animalName ;
-		
-		// Q2：フィールドに動物の数の変数を定義してください。
-		int animalNum ;
-		
-		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-		public Dog () {
-			// animalNameの変数に犬を代入する。
-			animalName = "犬";
-			
-		}
-		
-		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-		// コンストラクタに引数を設定。
-		public Dog (int num) {
-			// 受け取った値をanimalNumに代入する。
-			animalNum = num ;
-			
-		}
 
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String animalName ;
+
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int animalNum ;
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Dog () {
+		// animalNameの変数に犬を代入する。
+		this.animalName = "犬";
 
 	}
+
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	// コンストラクタに引数を設定。
+	public Dog (int animalNum) {
+		// 受け取った値をanimalNumに代入する。
+		this.animalNum = animalNum ;
+
+	}
+
+
+}
 
